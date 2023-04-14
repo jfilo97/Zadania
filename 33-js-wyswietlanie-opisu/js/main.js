fbf01ed1f0b95d595234232ce2c930dceea544a2
jfilo <j97filo@gmail.com>
2023-04-14 12:21:20 +0200

    zrobione zadanie 333 js-wyswietlanie opisu

---
+++ 33-js-wyswietlanie-opisu/js/main.js
@@ -0,0 +1,8 @@
+let paragrafOpis = document.getElementById("opis");
+
+let buttonOpis = document.querySelector("#button-opis");
+
+
+buttonOpis.onclick = function() {
+    paragrafOpis.innerText = "Opis produktu: Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+}

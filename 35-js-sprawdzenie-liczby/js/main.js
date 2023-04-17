638df3d4735e06551ec99cd5eeb893314007c0bf
jfilo <j97filo@gmail.com>
2023-04-17 14:20:08 +0200

    poprawione zadanie 35

--- 35-js-sprawdzenie-liczby/js/main.js
+++ 35-js-sprawdzenie-liczby/js/main.js
@@ -1,6 +1,7 @@
-let n = 7;
-if (n >= 100 && n <= 200) {
-    console.log("Liczba znajduje się w przedziale.");
+function checkNumber(n) {
+  if (n >= 100 && n <= 200) {
+    console.log("Liczba znajduje się w przedziale");
   } else {
-    console.log("Liczba nie znajduje się w przedziale.");
+    console.log("Liczba nie znajduje się w przedziale");
   }
+}
